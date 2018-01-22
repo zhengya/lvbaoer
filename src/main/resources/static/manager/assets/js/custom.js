3418f0e7138900b96d0f5062b843ab6187ac866a
coderZhengYaTeng <zhengyt@mpen.com.cn>
2018-01-22 17:38:34 +0800

    管理端页面

---
+++ src/main/resources/static/manager/assets/js/custom.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+    "use strict";
+    var mainApp = {
+        metisMenu: function () {
+            $('#main-menu').metisMenu();
+        },
+        loadMenu: function () {
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse')
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse')
+                }
+            });
+        }
+    };
+    $(document).ready(function () {
+        mainApp.metisMenu();
+        mainApp.loadMenu();
+    });
+}(jQuery));
